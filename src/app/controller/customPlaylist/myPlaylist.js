a39fc5dca67c94f2f6c293488e4d6d4e7bc4da39
SimJungUk <simju9397@gmail.com>
2019-07-11 03:35:57 +0900

    custom playlist

--- src/app/controller/customPlaylist/myPlaylist.js
+++ src/app/controller/customPlaylist/myPlaylist.js
@@ -2,35 +2,40 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('../../module/jwt');
 const returnCode = require('../../model/returnCode');
-const returnMessage = require('../../../config/returnMessage');
 const responseUtil = require('../../module/responseUtil');
 const playlistModules = require('../../module/playlistModules');
-const pool = require('../../module/pool');
 const playlist = require('../../model/schema/playlist');
-//myPlaylist 조회
+
 router.get('/', async (req, res) => {
+
     const ID = jwt.verify(req.headers.authorization);
 
     //회원일 경우
     if (ID > 0) {
         const myPlaylist = await playlistModules.searchMyPlaylist(ID);
+
         const customArray = myPlaylist.customPlaylist;
-        const customArtworksArray = new Array();
-        let songList = new Array();
-        for(var i = 0; i < customArray.length ; i++) {
-            songList[i] = await playlistModules.getSongList(customArray[i]);
-            customArtworksArray[i] = new Array();
-            for(var j = 0; j < 4 ; j++) {
-                customArtworksArray[i][j] =(songList[i][j]).artwork;
+        let result = new Array();
+
+        for (let i = 0; i < customArray.length; i++) {
+            let result1 = (await playlist.find({ _id: customArray[i] }))[0];
+            let songList = await playlistModules.getSongList(customArray[i]);
+            let thumbnail = new Array();
+
+            for (j = 0; j < songList.length; j++) {
+                thumbnail.push(songList[j].artwork);
             }
-        }
-        
-        const result = {
-            "custom": {
-                customArray : customArray,
-                thumbnail : customArtworksArray
+            let data = {
+                songList: result1.songList,
+                _id: result1._id,
+                playlistName: result1.playlistName,
+                playlistComment: result1.playlistComment,
+                userIdx: result1.userIdx,
+                thumbnail: thumbnail
             }
+            result.push(data);
         }
+
         res.status(200).send(responseUtil.successTrue(returnCode.OK, "myPlaylist 조회 성공", result))
     }
     //비회원일 경우
@@ -42,6 +47,5 @@ router.get('/', async (req, res) => {
         res.status(200).send(responseUtil.successFalse(returnCode.FORBIDDEN, "access denied"));
     }
 })
-
 
 module.exports = router;
