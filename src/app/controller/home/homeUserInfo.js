01031065366d724f66efbd3b640f53166b404fe7
SimJungUk <simju9397@gmail.com>
2019-07-10 17:16:07 +0900

    userInfo result edit

--- src/app/controller/home/homeUserInfo.js
+++ src/app/controller/home/homeUserInfo.js
@@ -7,19 +7,33 @@ const returnCode = require('../../model/returnCode');
 const returnMessage = require('../../../config/returnMessage');
 const responseUtil = require('../../module/responseUtil');
 
-router.get('/', async(req, res, next) => {
+router.get('/', async (req, res, next) => {
     const ID = await jwt.verify(req.headers.authorization);
 
-    if(ID > 0) { //회원일 경우
-        // 홈 상단 사용자 정보
-        userInfoQuery = 'SELECT nickname, hitSongCount, rateSongCount, totalPoint FROM user WHERE userIdx = ?';
-        userInfoResult = (await pool.queryParam_Arr(userInfoQuery, [ID]))[0];
-        res.status(200).send(responseUtil.successTrue(returnCode.OK, returnMessage.SUCCESS, userInfoResult));
-    }else if(ID == -1) { //비회원일 경우
-        res.status(200).send(responseUtil.successFalse(returnCode.NOT_FOUND, returnMessage.NOT_CORRECT_TOKEN_USER));
+    let result = {
+        nickname: "",
+        hitSongCount: 0,
+        rateSongCount: 0,
+        totalPoint: 0,
+        auth: false
+    };
+
+    if (ID > 0) {
+        const userInfoQuery = 'SELECT nickname, hitSongCount, rateSongCount, totalPoint FROM user WHERE userIdx = ?';
+        const userInfoResult = (await pool.queryParam_Arr(userInfoQuery, [ID]))[0];
+
+        result.nickname = userInfoResult.nickname;
+        result.hitSongCount = userInfoResult.hitSongCount;
+        result.rateSongCount = userInfoResult.rateSongCount;
+        result.totalPoint = userInfoResult.totalPoint;
+        result.auth = true;
+        
+        res.status(200).send(responseUtil.successTrue(returnCode.OK, returnMessage.SUCCESS, result));
+    } else if (ID == -1) { //비회원일 경우
+        res.status(200).send(responseUtil.successTrue(returnCode.OK, returnMessage.SUCCESS, result));
     }
     else { //토큰 검증 실패
-        res.status(200).send(responseUtil.successFalse(returnCode.FORBIDDEN, returnMessage.EMPTY_TOKEN));
+        res.status(200).send(responseUtil.successFalse(returnCode.UNAUTHORIZED, returnMessage.EMPTY_TOKEN));
     }
 });
 
